88581d19dff415c36fad6f230c16ff2289f6db8b
artem <mstrJesm@gmail.com>
2020-05-20 22:47:11 +0300

    finished random-planet component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Spinner from '../spinner';
 import PlanetView from './planet-view';
+import ErrorIndicator from '../error-indicator';
 import SwapiService from '../../services/swapi-service';
 
 import './random-planet.css';
@@ -18,45 +19,59 @@ class RandomPlanet extends Component {
       rotationPeriod: null,
       diameter: null
     },
-    loading: true
-  }
+    loading: true,
+    error: false
+  };
 
-  constructor() {
-    super();
-    this.updatePlanet();
+
+  componentDidMount() {
+    setInterval(() => {
+      this.updatePlanet();
+    }, 1500);
   }
+  
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+  
 
   onPlanetLoaded = (planet) => {
     this.setState({planet, loading: false})
-  }
+  };
 
   onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    });
+  };
 
-  }
-
-  updatePlanet() {
-   //const id = Math.floor(Math.random() * 25 + 2);
-    const id = 123123123;
+  updatePlanet = () => {
+    const id = Math.floor(Math.random() * 25) + 3;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
       .catch(this.onError)
-  }
+  };
 
   render() {
 
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
 
+    const hasDada = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet}/> : null;
-    
+    const content = hasDada ? <PlanetView planet={planet}/> : null;
+
     return(
       <div className="random-planet jumbotron rounded">
+        { errorMessage }
         { spinner }
         { content }
       </div> 
     );
-  }
+  };
 };
 
 export default RandomPlanet;
