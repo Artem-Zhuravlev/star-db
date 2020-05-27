94f23874e4f986327841fb8d06b4fed783689e02
artem <mstrJesm@gmail.com>
2020-05-27 22:00:45 +0300

    refactoring of code

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './header.css';
 
-const Header = () => {
+const Header = ({onServiceChange}) => {
   return(
-    <div className="header d-flex">
+    <div className="header d-flex align-items-center">
       <h3>
         <a href="#">Star DB</a>
       </h3>
-      <ul className="d-flex">
+      <ul className="d-flex mb-0">
         <li><a href="#">People</a></li>
         <li><a href="#">Planets</a></li>
         <li><a href="#">Starships</a></li>
       </ul>
+      <button 
+        className="btn btn-primary btn-sm"
+        onClick={onServiceChange}>Change service</button>
     </div>
   ); 
 };
 
-export default Header;
+export default Header; 
